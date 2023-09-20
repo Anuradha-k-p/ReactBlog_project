213cc7b645ed399a7bcfa0b91a350652942ce8b2
Anuradha-k-p <72305218+Anuradha-k-p@users.noreply.github.com>
2023-09-21 00:41:50 +0530

    Update Home.js

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,18 +1,63 @@
-import React,{useContext} from 'react'
-import {Store} from './ConAPI'
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react'
+import { Store } from './ConAPI'
 import arrowr from '../img/arrowr.svg'
-import '../App.css'
-function Hollywood() {
-  const [hollyData]=useContext(Store);
+
+import { Link } from 'react-router-dom'
+function Home() {
+
+  const [homeData]=useContext(Store)
   return (
+
     <>
 
-    {/* <div  id='letst'> */}
+
+    <div className='hmain'>
+<div className='hmain1'>
+<div className='hmain11'>
+{
+        homeData.filter((item)=>(item.cat==='Hollywood') && (item.id===17)).map((data,tech)=>{
+            return(
+                <>
+                <div className='Hcontainer' key={tech}>   
+                <img className='himg1' src={data.image} alt='Not Found'/>
+                <div className='caption'>
+                <h2>Pirates Caribbean</h2>
+                <p>{data.date}</p>
+                </div>
+                </div>
+                </>
+            )
+        })
+      }
+</div>
+<div className='hmain12'>
+{homeData.filter((item)=>((item.id===34)  ||  (item.id===44) )).map((data,tech)=>{
+            return(
+              
+                <div className='Hcontainer' key={tech}>
+                <img className='himg2' src={data.image} alt='Not Found'/>
+                <div className='caption1'>
+                <h3>Computer Technology
+                </h3>
+                <p>{data.date}</p>
+                </div>
+                <div className='caption2'>
+                <h3>Shahi paneer
+                </h3>
+                <p>{data.date}</p>
+                </div>
+                </div>
+              
+            )
+        })
+      }
+</div>
+</div>
+
 <h1 className='b2'>The Latest</h1>
-<div className='hmain2' >
-<div className='hmain21s' > 
-        {hollyData.filter((item)=>  (item.id===68)  ).map((data,home)=>{
+<div className='hmain2'>
+<div className='hmain21s'> 
+        {homeData.filter((item)=>  (item.id===63)  ).map((data,home)=>{
         return(
           <div className='hmain21'>
              <div   key={home}>
@@ -30,7 +75,7 @@ function Hollywood() {
       }
         </div>
         <div className='hmain21s'> 
-        {hollyData.filter((item)=> (item.id===69) ).map((data,home)=>{
+        {homeData.filter((item)=> (item.id===48) ).map((data,home)=>{
         return(
           <div>
              <div   key={home}>
@@ -48,7 +93,7 @@ function Hollywood() {
       }
         </div>
         <div className='hmain21s'>
-        {hollyData.filter((item)=> (item.id===70)).map((data,home)=>{
+        {homeData.filter((item)=> (item.id===29)).map((data,home)=>{
         return(
           <div>
              <div   key={home}>
@@ -67,171 +112,250 @@ function Hollywood() {
         </div>
 
 </div>
-{/* </div> */}
 
 
-    <div className='Outer'>
-    <div className='seccnt'>
-      <h1 className='b1'>Hollywood</h1>
-     {hollyData.filter((item)=>(item.cat==='Hollywood') && (item.id>=19)).map((data,bolly)=>{
-          return(
-              <>
-            
-              <div className='bcontainer' key={bolly}>
-                <div >
-              <img className='img' src={data.image} alt='Not Found'/>
-              </div>
-              <div className='dsc'>
-              <Link to={`/Detail/${data.id}`}> <h3>{data.Heading}</h3></Link>
-              <p className='ds'>{data.description}</p>
-              <p className='ds'>{data.date}</p>
+
+
+<h1 className='b2'>Latest Article</h1>
+
+<div className='hmain3'>
+<div className='hmain31'> 
+
+<div className='hmain311 latesArticle'> 
+
+{homeData.filter((item)=>((item.cat==='Hollywood') && (item.id>=26))).map((data,holly)=>{
+            return(
+                <>
               
-              </div>
-              
-              </div>
-              <hr className='hr'/>
-              </>
-               
-          )
-      })
-
-      } 
-     <p><span > Load More</span><img className='d' src={arrowr} alt='Not Found'/></p>
-      </div>
-      <div className='seccnt'>
-        <h1  className='b1'>Top Posts</h1>
-      {hollyData.filter((item)=>(item.cat==='Hollywood') && (item.id===16)).map((data,bolly)=>{
-          return(
-              <>
-            
-              <div className='blcontainer' key={bolly}>
-                <div >
-              <img className='bsimg' src={data.image} alt='Not Found'/>
-              </div>
-              <div className='bdsc'>
-              <span className='bsh'>{data.Heading}</span> <span id='st'  className='number'>1</span>
-              </div>
-              <p className='ds'>{data.date}</p>
-            <hr></hr>
-              </div>
-              </>
-          )
-      })
-    }
-
-{
-hollyData.filter((item)=>item.id===17).map((data,ind)=>{
-  return(
-    <div className='short' key={ind}>
-      <div className='bsimg'>
-       <img className='bsimg1' src={data.image} alt='Not Found'/>
-       </div>
-       <div className='bdsc2'>
-              <h3 className='bsh'>{data.Heading}</h3> 
-              <span className='ds'>{data.date}</span>
-              </div>
-              <div id='number'><span  className='number'>2</span></div>
-
-           
-    </div>
-  )
- 
-
-})
-}
-      <hr/>
-      {
-hollyData.filter((item)=>item.id===23).map((data,ind)=>{
-  return(
-    <div className='short' key={ind}>
-      <div className='bsimg'>
-       <img className='bsimg1' src={data.image} alt='Not Found'/>
-       </div>
-       <div className='bdsc2'>
-              <h3 className='bsh'>{data.Heading}</h3> 
-              <span className='ds'>{data.date}</span>
-              </div>
-              <div id='number'><span  className='number'>3</span></div>
-
-           
-    </div>
-  )
- 
-
-})
-}
-      <hr/>
-      {
-hollyData.filter((item)=>item.id===70).map((data,ind)=>{
-  return(
-    <div className='short' key={ind}>
-      <div className='bsimg'>
-       <img className='bsimg1' src={data.image} alt='Not Found'/>
-       </div>
-       <div className='bdsc2'>
-              <h3 className='bsh'>{data.Heading}</h3> 
-              <span className='ds'>{data.date}</span>
-              </div>
-              <div id='number'><span  className='number'>4</span></div>
-
-           
-    </div>
-  )
- 
-
-})
-}
-<hr/>
-      {
-hollyData.filter((item)=>item.id===24).map((data,ind)=>{
-  return(
-    <div className='short' key={ind}>
-      <div className='bsimg'>
-       <img className='bsimg1' src={data.image} alt='Not Found'/>
-       </div>
-       <div className='bdsc2'>
-              <h3 className='bsh'>{data.Heading}</h3> 
-              <span className='ds'>{data.date}</span>
-              </div>
-              <div id='number'><span  className='number'>5</span></div>
-
-           
-    </div>
-  )
- 
-
-})
-}
-<hr/>
-      {
-hollyData.filter((item)=>item.id===23).map((data,ind)=>{
-  return(
-    <div className='short' key={ind}>
-      <div className='bsimg'>
-       <img className='bsimg1' src={data.image} alt='Not Found'/>
-       </div>
-       <div className='bdsc2'>
-              <h3 className='bsh'>{data.Heading}</h3> 
-              <span className='ds'>{data.date}</span>
-              </div>
-              <div id='number'><span  className='number'>6</span></div>
-
-           
-    </div>
-  )
- 
-
-})
-} 
-<div className='Adds1'>
-Advertisement
+                <div className='bcontainer1' key={holly}>
+                  <div >
+                <img className='himg5' src={data.image} alt='Not Found'/>
+                </div>
+                <div className='dsc1'>
+                <Link to={`/Detail/${data.id}`}>  <h3>{data.Heading}</h3></Link>
+                <p className='ds1'>{data.description}</p>
+                </div>
+                </div>
+                </>
+            )
+        })
+      }
+  
 </div>
 
-      </div>
-      
+
+<div className='hmain311 latesArticle'> 
+
+{homeData.filter((item)=>((item.cat==='Food') && (item.id>=52))).map((data,holly)=>{
+            return(
+                <>
+              
+                <div className='bcontainer1' key={holly}>
+                  <div >
+                <img className='himg5' src={data.image} alt='Not Found'/>
+                </div>
+                <div className='dsc1'>
+                <Link to={`/Detail/${data.id}`}>  <h3>{data.Heading}</h3></Link>
+                <p className='ds1'>{data.description}</p>
+                </div>
+                </div>
+                </>
+            )
+        })
+      }
+  
+</div>
+
+
+<div className='hmain311 latesArticle'> 
+
+{homeData.filter((item)=>((item.cat==='Fitness') && (item.id>=66))).map((data,holly)=>{
+            return(
+                <>
+              
+                <div className='bcontainer1' key={holly}>
+                  <div >
+                <img className='himg5' src={data.image} alt='Not Found'/>
+                </div>
+                <div className='dsc1'>
+                <Link to={`/Detail/${data.id}`}>  <h3>{data.Heading}</h3></Link>
+                <p className='ds1'>{data.description}</p>
+                </div>
+                </div>
+                </>
+            )
+        })
+      }
+  
+</div>
+
+
+
+<div className='hmain311 latesArticle'> 
+<p><img  src={arrowr} alt='Not Found'/><span className='dm'> Load More</span></p>  
+{homeData.filter((item)=>((item.cat==='Bollywood') && (item.id>=14))).map((data,holly)=>{
+            return(
+                <>
+              
+                <div className='bcontainer1' key={holly}>
+                  <div >
+                <img className='himg5' src={data.image} alt='Not Found'/>
+                </div>
+                <div className='dsc1'>
+                <Link to={`/Detail/${data.id}`}>  <h3>{data.Heading}</h3></Link>
+                <p className='ds1'>{data.description}</p>
+                </div>
+                </div>
+                </>
+            )
+        })
+      }
+  
+</div>
+
+
+
+
+
+
+
+
+
+
+ 
+{/* <h1  className='b1'>Top Posts</h1> */}
+{/* <div className='hmain312'>
+{homeData.filter(((item)=>(item.id===27))).map((data)=>{
+            return(
+                <>            
+                <img className='himg6' src={data.image} alt='Not Found'/>
+                </>
+            )
+        })
+      }
+   </div> */}
+
+
+
+
+ 
+</div>
+{/*  
+<div className='hmain32'>
+  <div className='Adds'>
+ Advertisement
+ 
   </div>
- </>
+   */}
+  <div className='hmain322'>
+    <div>
+   <h1  className='b1'>Top Posts</h1>
+    {homeData.filter((item)=>(item.id===5)).map((data,tech)=>{
+            return(
+                <>
+                <div className='blcontainer' key={tech}>
+                  <Link className='Link' to={`/Detail/${data.id}`}>
+                  <div >
+
+                <img className='bsimg' src={data.image} alt='Not Found'/>
+                </div>
+                <div className='bdsc'>
+                <span className='bsh'>{data.Heading}</span> <span id='st'  className='number'>1</span>
+                </div>
+                </Link> 
+                <p className='ds'>{data.date}</p>
+              <hr></hr>
+                </div>
+                </>
+            )
+        })
+      }
+    </div>
+    <div>
+    {
+  homeData.filter((item)=>item.id===39).map((data,ind)=>{
+    return(
+     
+      <div className='shorthome' key={ind}>
+        <div className='bsimg'>
+         <img className='bsimg1' src={data.image} alt='Not Found'/>
+         </div>
+         <div className='bdsc2'>
+                <h3 className='bsh'>{data.Heading}</h3> 
+                <span className='ds'>{data.date}</span>
+                </div>
+                <div id='number'><span  className='number'>2</span>
+                </div>
+      </div>
+    )
+   
+
+  })
+}
+    </div>
+    <hr className='hrhome'/>
+    <div>
+    {
+  homeData.filter((item)=>item.id===61).map((data,ind)=>{
+    return(
+     
+      <div className='shorthome' key={ind}>
+        <div className='bsimg'>
+         <img className='bsimg1' src={data.image} alt='Not Found'/>
+         </div>
+         <div className='bdsc2'>
+                <h3 className='bsh'>{data.Heading}</h3> 
+                <span className='ds'>{data.date}</span>
+                </div>
+                <div id='number'><span  className='number'>3</span>
+                </div>
+      </div>
+    )
+   
+
+  })
+}
+    </div>
+    <hr/>
+    <div>
+    {
+  homeData.filter((item)=>item.id===19).map((data,ind)=>{
+    return(
+     
+      <div className='shorthome' key={ind}>
+        <div className='bsimg'>
+         <img className='bsimg1' src={data.image} alt='Not Found'/>
+         </div>
+         <div className='bdsc2'>
+                <h3 className='bsh'>{data.Heading}</h3> 
+                <span className='ds'>{data.date}</span>
+                </div>
+                <div id='number'><span  className='number'>4</span>
+                </div>
+      </div>
+    )
+   
+
+  })
+}
+    </div>
+ 
+  </div>
+   </div>
+    
+
+ 
+   <div className='hmain32'>
+  <div className='Adds'>
+ Advertisement
+ 
+  </div>
+
+   </div> 
+
+    </div>
+    </>
   )
 }
 
-export default Hollywood
+export default Home
