8281d65e6e20ea3d3d26034172e68f8639078ca2
Anuradha-k-p <72305218+Anuradha-k-p@users.noreply.github.com>
2023-09-25 22:24:46 +0530

    Update NavBarCompo.js

--- src/Component/NavBarCompo.js
+++ src/Component/NavBarCompo.js
@@ -1,33 +1,65 @@
-import React,{useState} from 'react'
-import '../App.css'
-import { Link} from 'react-router-dom'
-import {GiHamburgerMenu} from 'react-icons/gi'
-import {RxCross2} from 'react-icons/rx'
+import React, { useState } from 'react';
+import '../App.css';
+ import { NavLink} from 'react-router-dom';
+// import { GiHamburgerMenu } from 'react-icons/gi';
+// import { RxCross2 } from 'react-icons/rx';
+import "./Nav.css";
 
 function LinkCompo() {
-  const[isMobile,setIsMobile]=useState(false);
-  return (
-  <>
+  const [count,setCount] = useState(false);
 
-  <div className='hdl'>
-    <h1><span id="titlethe">THE </span><span id="titlesiren">SIREN</span></h1>
-    </div>
-    <button id='btn1' className='mobileview' onClick={()=>{setIsMobile(!isMobile)}}>
-        {isMobile ? <GiHamburgerMenu/> :  <RxCross2/> }
-        </button>
-        <p></p>
- <div  className={isMobile ? "cross" : "menu"} onClick={()=>{setIsMobile(false)}}>
- <Link to='/' className='nav_element'>Home</Link>
-  <Link to='/Bollywood' className='nav_element'>Bollywood</Link>
-  <Link to='/Technology' className='nav_element'>Technology</Link>
-  <Link to='/Hollywood' className='nav_element'>Hollywood</Link>
-  <Link to='/Fitness' className='nav_element'>Fitness</Link>
-  <Link to='/Food' className='nav_element'>Food</Link>
-    </div>
-        <hr id="hr"></hr>
-   
+  return (
+    <>
+      <div className='hdl'>
+        <h1>
+          <span id="titlethe">THE </span>
+          <span id="titlesiren">SIREN</span>
+        </h1>
+      </div>
+     
+
+
+<div id="navlink">
+    <ul>
+        <li>
+    <NavLink  to="/" className="link" >Home</NavLink></li>
+        <li>
+    <NavLink  to="/Hollywood" className="link" >Hollywood</NavLink></li>
+        <li>
+    <NavLink  to="/Bollywood" className="link" >Bollywood</NavLink></li>
+        <li>
+    <NavLink  to="/Technology"  className="link">Technology </NavLink></li>
+        <li>
+    <NavLink  to="/Fitness"  className="link">Fitness</NavLink></li>
+        <li>
+        <NavLink  to="/Food"  className="link">Food</NavLink></li>
+        
+</ul>
+<div onClick={()=>setCount(!count)} className="display"><i className="fa-solid fa-bars"  ></i></div>
+
+
+</div>
+
+<div className={(count)?"hambergerlinks show":"hambergerlinks hide"}>
+<ul>
+        <li>
+    <NavLink  onClick={()=>setCount(!count)} to="/" className="link" >Home</NavLink></li>
+        <li>
+    <NavLink   onClick={()=>setCount(!count)}  to="/Hollywood" className="link" >Hollywood</NavLink></li>
+        <li>
+    <NavLink   onClick={()=>setCount(!count)}  to="/Bollywood" className="link" >Bollywood</NavLink></li>
+        <li>
+    <NavLink   onClick={()=>setCount(!count)}  to="/Technology"  className="link">Technology </NavLink></li>
+        <li>
+    <NavLink   onClick={()=>setCount(!count)}  to="/Fitness"  className="link">Fitness</NavLink></li>
+        <li>
+        <NavLink   onClick={()=>setCount(!count)}  to="/Food"  className="link">Food</NavLink></li>
+        
+</ul>
+</div>
+
     </>
-  )
+  );
 }
 
-export default LinkCompo
+export default LinkCompo;
