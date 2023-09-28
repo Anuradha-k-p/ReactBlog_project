89ae134b25700f344bdc65cb29e7a766fe115dc0
Anuradha-k-p <72305218+Anuradha-k-p@users.noreply.github.com>
2023-09-28 09:17:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+
 import RouteCompo from './Component/RouteCompo';
 import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
     <>
+<div className='hdl'>
+<h1>
+<span id="titlethe">THE </span>
+          <span id="titlesiren">SIREN</span>
+        </h1>
+        </div>
  
     <BrowserRouter>
     
     <RouteCompo/>
+  
     </BrowserRouter>
   
     </>
